7b46eb03104c03f0d488b86daa7cdd77aa5e7f89
Joshua Gold <jigold@pennmedicine.upenn.edu>
2018-07-06 17:14:51 -0400

    Eyelink starting to work, still need to drift correct

--- Tasks/ModularTasks/Testing/testDotsReadableEyeEyelink.m
+++ Tasks/ModularTasks/Testing/testDotsReadableEyeEyelink.m
@@ -1,7 +1,7 @@
 % Script for testing the dotsReadableEyeEyelink class
 
 % set up a small screen for calibration
-screenEnsemble = makeScreenEnsemble(false, 0);
+screenEnsemble = makeScreenEnsemble(false, 1);
 screenEnsemble.callObjectMethod(@open);
 
 % Get the readableEye object
@@ -13,28 +13,31 @@ e.screenEnsemble = screenEnsemble;
 % Run initial calibration routine
 e.calibrate();
    
-try
-   
-   % Run initial calibration routine
-   e.calibrate();
-   
-   % Open the gaze monitor
-   eyeGUI(topsTreeNode('test'), e);   
-   
-   % Don't buffer, don't recenter
-   resetGaze(e, false, false);
-   for ii = 1:100
-      e.read();
-      pause(0.1);
-   end
-   
-   % Buffer and recenter
-   resetGaze(e, true, true);
-   for ii = 1:100
-      e.read();
-      pause(0.1);
-   end
-      
-catch
-   dotsTheScreen.closeWindow();
-end
+% % try validation
+e.calibrate('v');
+
+% try
+%    
+%    % Run initial calibration routine
+%    e.calibrate();
+%    
+%    % Open the gaze monitor
+%    eyeGUI(topsTreeNode('test'), e);   
+%    
+%    % Don't buffer, don't recenter
+%    resetGaze(e, false, false);
+%    for ii = 1:100
+%       e.read();
+%       pause(0.1);
+%    end
+%    
+%    % Buffer and recenter
+%    resetGaze(e, true, true);
+%    for ii = 1:100
+%       e.read();
+%       pause(0.1);
+%    end
+%       
+% catch
+%    dotsTheScreen.closeWindow();
+% end
