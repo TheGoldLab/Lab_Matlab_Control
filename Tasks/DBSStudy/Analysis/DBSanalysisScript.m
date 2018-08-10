a65703b5858de240067d91b12a4685ffd01527e2
Joshua Gold <jigold@mail.med.upenn.edu>
2018-08-10 08:44:29 -0400

    updated DBS analysis tools

--- Tasks/DBSStudy/Analysis/DBSanalysisScript.m
+++ Tasks/DBSStudy/Analysis/DBSanalysisScript.m
@@ -2,5 +2,63 @@
 %
 % Created 6/18/2018 by jig
 
-testFilename = 'data_2018_06_19_08_36';
+testFilename = 'data_2018_08_08_08_55';
 
+FIRA = DBSmakeFIRA(testFilename);
+
+figure
+
+% timing indices
+sgis = cat(2, ...
+   find(strcmp(FIRA.ecodes.name, 'time_targsOn'), 1), ...
+   find(strcmp(FIRA.ecodes.name, 'time_dotsOn'), 1), ...
+   find(strcmp(FIRA.ecodes.name, 'time_targsOff'), 1), ...
+   find(strcmp(FIRA.ecodes.name, 'time_fixOff'), 1), ...
+   find(strcmp(FIRA.ecodes.name, 'time_dotsOff'), 1), ...
+   find(strcmp(FIRA.ecodes.name, 'time_fdbkOn'), 1));
+rti = find(strcmp(FIRA.ecodes.name, 'RT'), 1);
+
+% task indices
+%  1  = VGS
+%  2  = MGS
+%  3  = Quest dots
+%  7  = Speed, no bias
+%  10 = Accuracy, no bias
+tis = [1 2 3 7 10];
+nts = length(tis);
+lm  = 15;
+clf
+for tt = 1:nts
+   
+   for ii = find(FIRA.ecodes.data(:,1)==tis(tt))'
+      
+      xax   = FIRA.analog.data{ii}(:,1);
+      xs    = FIRA.analog.data{ii}(:,2);
+      ys    = FIRA.analog.data{ii}(:,3);
+      Lgood = xax>=(FIRA.ecodes.data(ii,sgis(1))-200) & xax<=FIRA.ecodes.data(ii,sgis(6));
+            
+      % get index of saccade soon after RT
+      if ecodes.data(tt,1) <= 2
+         refTime = FIRA.ecodes.data(ii,sgis(4)); % Fix off for VGS/MGS
+      else
+         refTime = FIRA.ecodes.data(ii,sgis(2)); % Dots on
+      end
+      fixIndex    = find(xax<=FIRA.ecodes.data(ii,sgis(4)),1);
+      sacEndIndex = find(xax<=(refTime+FIRA.ecodes.data(tt,rti)+50),1);
+
+      % x vs y
+      subplot(nts, 2, (tt-1)*2+1); hold on;
+      plot([-lm lm], [0 0], 'k:');
+      plot([0 0], [-lm lm], 'k:');
+      plot(xs(Lgood), ys(Lgood), 'k-');
+      plot(xs(fixIndex), ys(fixIndex), 'go');
+      plot(xs(sacEndIndex), ys(sacEndIndex), 'go', 'MarkerFaceColor', 'g');
+      axis([-lm lm -lm lm]);
+      
+      % x,y vs t
+      subplot(nts, 2, (tt-1)*2+2); hold on;
+      plot([-1 6], [0 0], 'k:');
+      plot(xax(Lgood), xs(Lgood), 'r-');
+      plot(xax(Lgood), ys(Lgood), 'b-');
+   end
+end
--- Tasks/DBSStudy/Analysis/DBSmakeFIRA.m
+++ Tasks/DBSStudy/Analysis/DBSmakeFIRA.m
@@ -1,5 +1,5 @@
-function DBSmakeFIRA(filename)
-% function DBSmakeFIRA(filename)
+function FIRA = DBSmakeFIRA(filename)
+% function FIRA = DBSmakeFIRA(filename)
 %
 % Make a FIRA data struct from the raw/pupil data of a DBS experiment. 
 %
@@ -10,76 +10,63 @@ function DBSmakeFIRA(filename)
 % Created 5/26/18 by jig
 
 %% Parse filenames
+%
 if nargin < 1 || isempty(filename)
    % for debugging
    filename = 'data_2018_08_08_08_55';   
 end
 
-% Where the data are stored
-DBSfilepath = fullfile(dotsTheMachineConfiguration.getDefaultValue('dataPath'), 'DBSStudy');
+% Flush the data log
+topsDataLog.flushAllData();
 
 % Use the machine-specific data pathname to find the data
+DBSfilepath = fullfile(dotsTheMachineConfiguration.getDefaultValue('dataPath'), 'DBSStudy');
 rawFile = fullfile(DBSfilepath, 'Raw', [filename '.mat']);
+eyeFile = fullfile(DBSfilepath, 'Pupil', [filename '_eye']);
 
 %% Get the ecode matrix using the topsDataLog utility
 %
-% First flush the data log
-topsDataLog.flushAllData();
-
 % get the mainTreeNode
 mainTreeNodeStruct = topsDataLog.getTaggedData('mainTreeNode', rawFile);
 mainTreeNode = mainTreeNodeStruct.item;
 
 % Now read the ecodes -- note that this works only if the trial struct was
 % made only with SCALAR entries
-ecodes = topsDataLog.parseEcodes('trial');
+FIRA.ecodes = topsDataLog.parseEcodes('trial');
 
+%% Get the gaze data
+%
+% Use constructor class static method to read the data file.
+%
+% for PupilLabs: Uses a python script...
+ui = mainTreeNode.nodeData{'Control'}{'userInputDevice'};
+[eyeData, tags] = feval([class(ui) '.readDataFromFile'], eyeFile);
+eyeData = cell2num(eyeData);
+
+%% Synchronize timing
+%
 % Get the number of trials
-numTrials = size(ecodes.data, 1);
+numTrials = size(FIRA.ecodes.data, 1);
    
 % Get indices of columns, by name
 %
 % indices of columns with local/screen/ui start times
-tli = find(strcmp(ecodes.name, 'time_local_trialStart'), 1);
-tui = find(strcmp(ecodes.name, 'time_ui_trialStart'), 1);
-tsi = find(strcmp(ecodes.name, 'time_screen_trialStart'), 1);
+tli = find(strcmp(FIRA.ecodes.name, 'time_local_trialStart'), 1);
+tui = find(strcmp(FIRA.ecodes.name, 'time_ui_trialStart'), 1);
+tsi = find(strcmp(FIRA.ecodes.name, 'time_screen_trialStart'), 1);
 
 % indices of times in screen reference frame
-sfi = find(strcmp(ecodes.name, 'time_fixOn'), 1);
+sfi = find(strcmp(FIRA.ecodes.name, 'time_fixOn'), 1);
 sgis = cat(2, ...
-   find(strcmp(ecodes.name, 'time_targsOn'), 1), ...
-   find(strcmp(ecodes.name, 'time_dotsOn'), 1), ...
-   find(strcmp(ecodes.name, 'time_targsOff'), 1), ...
-   find(strcmp(ecodes.name, 'time_fixOff'), 1), ...
-   find(strcmp(ecodes.name, 'time_dotsOff'), 1), ...
-   find(strcmp(ecodes.name, 'time_fdbkOn'), 1));
+   find(strcmp(FIRA.ecodes.name, 'time_targsOn'), 1), ...
+   find(strcmp(FIRA.ecodes.name, 'time_dotsOn'), 1), ...
+   find(strcmp(FIRA.ecodes.name, 'time_targsOff'), 1), ...
+   find(strcmp(FIRA.ecodes.name, 'time_fixOff'), 1), ...
+   find(strcmp(FIRA.ecodes.name, 'time_dotsOff'), 1), ...
+   find(strcmp(FIRA.ecodes.name, 'time_fdbkOn'), 1));
 
 % indices of times in ui reference frame
-uci = find(strcmp(ecodes.name, 'time_choice'), 1);
-
-%% Get the gaze data
-%
-% get ui device
-groupMap = datatubStruct.item.allGroupsMap('Control');
-ui = groupMap('userInputDevice');
-fileWithPath = fullfile(DBSfilepath, 'Pupil', [filename '_eye']);
-
-% Use constructor class static method to read the data file.
-%
-% for PupilLabs: Uses a python script...
-[eyeData, tags] = feval([class(ui) '.readDataFromFile'], fileWithPath);
-eyeData = cell2num(eyeData);
-
-% FOR DEBUGGING
-% load('tmp_pupil_data');
-% dataStruct = cat(1, [gaze_positions{:}]);
-% pos = [dataStruct.norm_pos];
-% eyeData = [ ...
-%    [dataStruct.timestamp]', ...
-%    pos(1:2:end)', pos(2:2:end)', ...
-%    [dataStruct.confidence]',];
-% tags = {'time', 'gaze_x', 'gaze_y', 'confidence'};
-% numSamples = size(eyeData, 1);
+uci = find(strcmp(FIRA.ecodes.name, 'time_choice'), 1);
 
 % get data indices
 eti = find(strcmp(tags, 'time'));
@@ -87,20 +74,18 @@ exi = find(strcmp(tags, 'gaze_x'));
 eyi = find(strcmp(tags, 'gaze_y'));
 eci = find(strcmp(tags, 'confidence'));
 
-%% Convert timestamps to local time
-%
 % Analog data
-eyeData(:,eti) = syncAnalogTimes(eyeData(:,eti), ecodes.data(:,[tli tui]));
+eyeData(:,eti) = syncAnalogTimes(eyeData(:,eti), FIRA.ecodes.data(:,[tli tui]));
 
 % Ecodes are all encoded with respect to fixaton onset
-wrtTimes = ecodes.data(:, sfi);
+wrtTimes = FIRA.ecodes.data(:, sfi);
 
 % Ecodes collected in screen time
-ecodes.data(:,sgis) = ecodes.data(:,sgis) - repmat(wrtTimes, 1, numel(sgis));
+FIRA.ecodes.data(:,sgis) = FIRA.ecodes.data(:,sgis) - repmat(wrtTimes, 1, numel(sgis));
 
 % Ecodes collected in ui time (here just choice)
-ecodes.data(:,uci) = (ecodes.data(:,uci) - ecodes.data(:,tui)) - ...
-   (wrtTimes - ecodes.data(:,tsi));
+FIRA.ecodes.data(:,uci) = (FIRA.ecodes.data(:,uci) - FIRA.ecodes.data(:,tui)) - ...
+   (wrtTimes - FIRA.ecodes.data(:,tsi));
 
 %% Get the pupil calibration data. Each item is:
 %  1. timestamp
@@ -121,10 +106,15 @@ end
 % For each trial (row in ecodes), make a cell array of pupil data
 %  timestamp, gaze x, gaze y, confidence, and re-code time wrt to fp onset.
 %  Also put everything in local time, wrt fp onset
-trialData = cell(numTrials, 1);
+FIRA.analog = struct(     ...
+        'name',         {tags}, ... % 1xn" cell array of strings
+        'acquire_rate', [],   ... % 1xn array of #'s, in Hz 
+        'store_rate',   round(1/median(diff(eyeData(:,1)))),   ... % 1xn" array of #'s, in Hz
+        'error',        {{}}, ... % mx1  array of error messages
+        'data',         {cell(numTrials,1)});      % mxn" cell array
 
 % Get list of local start times
-localTrialStartTimes = [ecodes.data(:,tli); inf];
+localTrialStartTimes = [FIRA.ecodes.data(:,tli); inf];
 
 for tt = 1:numTrials
    
@@ -133,42 +123,42 @@ for tt = 1:numTrials
       eyeData(:,eti) < localTrialStartTimes(tt+1);
    
    % Put eye data in order: time, x, y, confidence and in local time
-   trialData{tt} = eyeData(Lgaze, [eti exi eyi eci]);
+   FIRA.analog.data{tt} = eyeData(Lgaze, [eti exi eyi eci]);
 
    % Calibrate timestamps   
-   trialData{tt}(:,1) = (trialData{tt}(:,1) - ecodes.data(tt,tli)) - ...
-      (wrtTimes(tt) - ecodes.data(tt,tsi));
+   FIRA.analog.data{tt}(:,1) = (FIRA.analog.data{tt}(:,1) - FIRA.ecodes.data(tt,tli)) - ...
+      (wrtTimes(tt) - FIRA.ecodes.data(tt,tsi));
 end
-
-%% Plot stuff
-%
-for tt = 1:numTrials
-   
-   cla reset; hold on;
-   xax = trialData{tt}(:,1);
-   plot(xax, trialData{tt}(:,2), 'ko-');
-   plot(xax, trialData{tt}(:,3), 'ro-');
-   if size(trialData{tt}, 2) >= 4
-      Llo = trialData{tt}(:,4) < 0.7;
-      plot(xax(Llo), trialData{tt}((Llo),2), 'kx');
-      plot(xax(Llo), trialData{tt}((Llo),3), 'rx');
-   end
-   
-   % Show fpoff, RT
-   rti = find(strcmp(ecodes.name, 'RT'), 1);
-   if ecodes.data(tt,1) <= 2
-      refTime = ecodes.data(tt,sgis(4)); % Fix off for VGS/MGS
-   else
-      refTime = ecodes.data(tt,sgis(2)); % Dots on
-   end
-   plot([0 0], [-5 5], 'k--');
-   plot(ecodes.data(tt,sgis(1)).*[1 1], [-5 5], 'c-'); % trgs on
-   plot(refTime.*[1 1], [-5 5], 'b-'); % fp off/dots on
-   plot(ecodes.data(tt,uci).*[1 1]+.01, [-5 5], 'm-'); % choice
-   plot((refTime+ecodes.data(tt,rti)).*[1 1], [-5 5], 'g-'); % RT
-   plot(ecodes.data(tt,sgis(5)).*[1 1], [-5 5], 'b--'); % dots off
-   
-   ylim([-10 10])
-   r = input('next')
-end
+% 
+% %% Plot stuff
+% %
+% for tt = 1:numTrials
+%    
+%    cla reset; hold on;
+%    xax = FIRA.analog.data{tt}(:,1);
+%    plot(xax, FIRA.analog.data{tt}(:,2), 'ko-');
+%    plot(xax, FIRA.analog.data{tt}(:,3), 'ro-');
+%    if size(FIRA.analog.data{tt}, 2) >= 4
+%       Llo = FIRA.analog.data{tt}(:,4) < 0.7;
+%       plot(xax(Llo), FIRA.analog.data{tt}((Llo),2), 'kx');
+%       plot(xax(Llo), FIRA.analog.data{tt}((Llo),3), 'rx');
+%    end
+%    
+%    % Show fpoff, RT
+%    rti = find(strcmp(FIRA.ecodes.name, 'RT'), 1);
+%    if ecodes.data(tt,1) <= 2
+%       refTime = FIRA.ecodes.data(tt,sgis(4)); % Fix off for VGS/MGS
+%    else
+%       refTime = FIRA.ecodes.data(tt,sgis(2)); % Dots on
+%    end
+%    plot([0 0], [-5 5], 'k--');
+%    plot(FIRA.ecodes.data(tt,sgis(1)).*[1 1], [-5 5], 'c-'); % trgs on
+%    plot(refTime.*[1 1], [-5 5], 'b-'); % fp off/dots on
+%    plot(FIRA.ecodes.data(tt,uci).*[1 1]+.01, [-5 5], 'm-'); % choice
+%    plot((refTime+FIRA.ecodes.data(tt,rti)).*[1 1], [-5 5], 'g-'); % RT
+%    plot(FIRA.ecodes.data(tt,sgis(5)).*[1 1], [-5 5], 'b--'); % dots off
+%    
+%    ylim([-10 10])
+%    r = input('next')
+% end
 
