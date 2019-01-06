3af55c1d274101b9e8f5a249c4dbefe199dbb57d
Joshua Gold <jigold@mail.med.upenn.edu>
2019-01-06 15:29:29 -0500

    added more checks to ainscan

--- snow-dots/demos/readable/demoReadableEyeEOG.m
+++ snow-dots/demos/readable/demoReadableEyeEOG.m
@@ -10,7 +10,7 @@ m = dotsReadableEyeEOG();
 m.openGazeMonitor();
 
 % % Open the screen
-screen = dotsTheScreen.makeEnsemble(true, 1);
+screen = dotsTheScreen.makeEnsemble(false, 0);
 screen.callObjectMethod('open');
 % 
 % % Calibrate
