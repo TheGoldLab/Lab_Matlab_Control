aa02c70dce08d13be6504323fdd2890460251339
TheGoldLab Psychophysics <joshuagold@joshuas-mini.uphs.upenn.edu>
2019-11-25 15:42:49 -0500

    fix reproducibility bug in dots

--- snow-dots/classes/drawable/dotsDrawableDotKinetogram.m
+++ snow-dots/classes/drawable/dotsDrawableDotKinetogram.m
@@ -210,7 +210,8 @@ classdef dotsDrawableDotKinetogram < dotsDrawableVertices
             if self.coherenceSTD==0
                cohCoinToss = 100*self.thisRandStream.rand(1, nFrameDots) < self.coherence;
             else
-               coh = normrnd(self.coherence, self.coherenceSTD);
+%                coh = normrnd(self.coherence, self.coherenceSTD);
+               coh = self.coherence + self.coherenceSTD .* self.thisRandStream.randn();
                if coh < 0
                   if self.flipDir
                      degreeOffset = 180;
